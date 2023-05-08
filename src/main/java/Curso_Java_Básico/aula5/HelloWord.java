15124b78e56048e20b3b837abc724713d2a125d7
RafaelXimbs <rafa.spereira@icloud.com>
2023-05-08 18:47:13 -0300

    Primeiro Commit

---
+++ src/main/java/Curso_Java_Básico/aula5/HelloWord.java
@@ -0,0 +1,7 @@
+package Curso_Java_Básico.aula5;
+
+public class HelloWord {
+  public static void main (String[] args){
+    System.out.println("Hello, World");
+  }
+}
---
+++ src/main/java/Curso_Java_Básico/aula5/OlaMundo.java
@@ -0,0 +1,8 @@
+package Curso_Java_Básico.aula5;
+
+public class OlaMundo {
+  public static void main(String[] args){
+    System.out.println("Olá, Mundo!");
+  }
+  
+}
